226c9f8c500497d286288ffc8e574ddd0e250ec6
RobGThai <thisisrobg@gmail.com>
2016-09-19 20:32:17 +0700

    Add css.

--- src/components/ListingItem.js
+++ src/components/ListingItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import './ListingItem.css'
 
 function ListingItem(props) {
   const listing = props.listing
   return (
-    <div>
-      <img src={listing.thumbnail} />
-      <p>{listing.title}</p>
-      <p>{listing.highlight}</p>
+    <div className="itemStyle">
+      <table>
+        <tbody>
+          <tr>
+            <td><img className="imageStyle" src={listing.thumbnail} /></td>
+            <td>{listing.title}</td>
+          </tr>
+          <tr>
+            <td colSpan="2" className="secStyle">{listing.highlight}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
